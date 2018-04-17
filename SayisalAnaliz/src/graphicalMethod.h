88aa7ddde5434d2799e676ef93d7d33359ca64df
batuhanfaik <batuhanfaik@batuhan-toshiba>
2018-04-17 22:58:04 +0300

    Added graphicalMethod.h

---
+++ SayisalAnaliz/src/graphicalMethod.h
@@ -0,0 +1,70 @@
+/*
+ * graphicalMethod.h
+ *
+ *  Created on: Apr 17, 2018
+ *      Author: batuhanfaik
+ */
+
+#ifndef GRAPHICALMETHOD_H_
+#define GRAPHICALMETHOD_H_
+
+void graphicalMethod() {
+	//Degiskenler
+	double xVal1, xVal2, epsilon;
+	int degree;
+
+	//Girdiler
+	printf("Kacinci dereceden fonksiyonun kokunu ariyorsunuz?: ");
+	scanf("%d", &degree);
+
+	//Katsayilar dizisi
+	double coefficient[degree + 1];
+	for (int i = degree; i >= 0; i--) {
+		printf("%d. derecenin katsayisi: ", i);
+		scanf("%lf", &coefficient[i]);
+	}
+
+	//Girdiler
+	printf("Baslangic X1 ve X2 degerleri: ");
+	scanf("%lf %lf", &xVal1, &xVal2);
+
+	printf("Epsilon degeri: ");
+	scanf("%lf", &epsilon);
+
+	//Fonksiyon
+	double f(double xval) {
+		double value = 0;
+		double thisx = 1;
+
+		//Fonksiyonun hesaplandigi dongu
+		for (int i = 0; i < (degree + 1); i++) {
+			value = coefficient[i] * thisx + value;
+			thisx = thisx * xval;
+		}
+		return value;
+	}
+
+	//Ilk ortanca deger atamasi
+	double xValMid = (xVal1 + xVal2) / 2;
+
+	//Hesaplama Dongusu
+	while (fabs(f(xValMid)) > epsilon) {
+
+		//Eger kok birinci ve ortanca deger arasindaysa yeni ortanca ikinci deger
+		if (f(xVal1) * f(xValMid) < 0) {
+			xVal2 = xValMid;
+		}
+		//Eger kok birinci ve ortanca deger arasinda degilse yeni ortanca birinci deger
+		else {
+			xVal1 = xValMid;
+		}
+
+		//Yeni ortanca hesabi
+		xValMid = (xVal1 + xVal2) / 2;
+	}
+
+	//Cikti
+	printf("Fonksiyonun koku: %lf", xValMid);
+}
+
+#endif /* GRAPHICALMETHOD_H_ */
---
+++ SayisalAnaliz/src/main.c
@@ -0,0 +1,31 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 17, 2018
+ *      Author: batuhanfaik
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "graphicalMethod.h"
+
+int main() {
+	//Degisken tanimlari
+	int islem = 0;
+
+	//Islem secimi
+	printf("1-Grafik metoduyla kök bulma\n"
+			"Yapmak istediginiz islemin numarasini giriniz: ");
+	scanf("%d", &islem);
+
+	switch (islem) {
+	case (1):
+		graphicalMethod();
+		break;
+	default:
+		printf("Islem secilmedi.");
+	}
+
+	return 0;
+}
